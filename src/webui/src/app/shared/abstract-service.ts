f64c5f60ca50e2f9914038786a2a2e850471b0aa
emrealtun <emrealtun5444@gmail.com>
2020-08-31 22:18:52 +0300

    fatura detay geliştirmeleri tamamlanmıştır.

--- src/webui/src/app/shared/abstract-service.ts
+++ src/webui/src/app/shared/abstract-service.ts
@@ -1,6 +1,6 @@
 export abstract class AbstractService {
 
-    //readonly BASE_URL = 'http://78.186.68.95:8090/api';
-    readonly BASE_URL = 'http://localhost:8090/api';
+    readonly BASE_URL = 'http://78.186.68.95:8090/api';
+    //readonly BASE_URL = 'http://localhost:8090/api';
 
 }
