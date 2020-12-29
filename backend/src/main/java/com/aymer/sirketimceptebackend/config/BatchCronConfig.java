7eba0854ae1ab251ff87f78dcd5b39621b3706d5
emrealtun <RAG48cag>
2020-12-29 09:59:06 +0300

    hata düzenlemeleri.

--- backend/src/main/java/com/aymer/sirketimceptebackend/config/BatchCronConfig.java
+++ backend/src/main/java/com/aymer/sirketimceptebackend/config/BatchCronConfig.java
@@ -26,9 +26,9 @@ public class BatchCronConfig {
     @Autowired
     private ApplicationContext context;
 
-    @Scheduled(cron = "0 0/30 8 * * *")
+    @Scheduled(cron="0 0 8 * * *", zone="Europe/Istanbul")
     //@Scheduled(cron = "*/10 * * * * *")
-    public void siparisBildirimCronConf() throws Exception {
+    public void sirketBildirimCronConf() throws Exception {
         JobParameters params = new JobParametersBuilder()
                 .addString("JobID", String.valueOf(System.currentTimeMillis()))
                 .toJobParameters();
